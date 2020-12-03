7e23e4f8f7aa8b85946df89fb5ba79ebad427a54
luster02 <bombolti_02@hotmail.com>
2020-12-03 10:29:53 -0600

    [ADD]: layout

---
+++ src/layouts/index.jsx
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react'
+import { Helmet } from 'react-helmet'
+import { GlobalStyle } from '../styles/global.styles'
+import { Logo } from '../components/Logo'
+import { Navbar } from '../components/Navbar'
+
+const MainLayout = ({ children, path, showNavbar = true }) => {
+    return (
+        <Fragment>
+            <Helmet>
+                <noscript>this app needs javascript for run</noscript>
+            </Helmet>
+            <GlobalStyle />
+            <Logo path={path} />
+            {children}
+            {showNavbar && <Navbar />}
+        </Fragment>
+    )
+}
+
+export default MainLayout
