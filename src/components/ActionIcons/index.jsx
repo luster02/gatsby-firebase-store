efa0c407be747d1486739af4df3ac04150d6223e
jonathan <bombolti_02@hotmail.com>
2020-12-03 18:35:05 -0600

    [ADD]: conditional showActionIcon

--- src/components/ActionIcons/index.jsx
+++ src/components/ActionIcons/index.jsx
@@ -3,18 +3,23 @@ import { navigate } from 'gatsby'
 import { MdShoppingCart, MdHome } from 'react-icons/md'
 import { Icon } from './styles'
 
-const ShoppingCarIcon = () => < Icon ><MdShoppingCart size="32px" /></Icon>
+const ShoppingCarIcon = () => < Icon onClick={() => navigate('/cart')} ><MdShoppingCart size="32px" /></Icon>
 const HomeIcon = () => <Icon onClick={() => navigate('/')}> <MdHome size="32px" /> </Icon>
 
-export const ActionIcons = ({ path = "/" }) => {
-    switch (path) {
-        case "/":
-            return <ShoppingCarIcon />
+export const ActionIcons = ({ path = "/", show=true }) => {
 
-        case "/details/":
-            return <HomeIcon />
-
-        default:
-            return <ShoppingCarIcon />
+    function conditionalIcon() {     
+        switch (path) {
+            case "/":
+                return <ShoppingCarIcon />
+    
+            case "/details/":
+                return <HomeIcon />
+    
+            default:
+                return <ShoppingCarIcon />
+        }
     }
+
+    return show && conditionalIcon()
 }
--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -4,14 +4,14 @@ import { GlobalStyle } from '../styles/global.styles'
 import { Logo } from '../components/Logo'
 import { Navbar } from '../components/Navbar'
 
-const MainLayout = ({ children, path, showNavbar = true }) => {
+const MainLayout = ({ children, path, showNavbar = true, showActionIcon=true }) => {
     return (
         <Fragment>
             <Helmet>
                 <noscript>this app needs javascript for run</noscript>
             </Helmet>
             <GlobalStyle />
-            <Logo path={path} />
+            <Logo path={path} showActionIcon={showActionIcon} />
             {children}
             {showNavbar && <Navbar />}
         </Fragment>
